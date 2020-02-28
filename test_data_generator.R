0e88614a901e2c27ab1823604b5a1b99b7e57643
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-02-28 17:52:05 +0100

    add whitening option with spectrum

--- test_data_generator.R
+++ test_data_generator.R
@@ -2,8 +2,9 @@ source("data_generator.R")
 
 fs=4096
 duration=1
-ampl=5
+ampl=10
 fcut=15
+n=duration*fs
 
 ### signal ###
 s20 = read.table("inputs/s20-gw_10kpc16384.dat"); # V1 time, V2 signal
@@ -14,9 +15,15 @@ signalx=decimate(s20$V1,16384/fs)
 wvf.df = data.frame("V1"=signalx,"V2"=signaly)
 
 ### generate noisy data and signal buried ###
-d = data_generator(fs, duration, wvf.df, ampl, filtering="whitening", fcut=fcut, actPlot=TRUE)
+d = data_generator(fs, duration, wvf.df, ampl, filtering="HP", fcut=fcut, actPlot=TRUE)
 sprintf("noise and signal is whitened. Standard deviation: %g", sqrt(var(d$y)))
 
-dp = data_generator(fs, duration, wvf.df, ampl, filtering="HP", fcut=fcut, actPlot=TRUE)
+freq1 = fs*fftfreq(n)          # two-sided frequency vector
+freq = freq1[1:int(n/2)]
+wbFT = sqrt(fs)*fft(d$y)/(n*sqrt(n));
+
+plot(freq,2*abs(wbFT)[1:int(n/2)], log="y",type="l", xlab="frequency",ylab="ASD", col="cyan",ylim=c(1e-24,1e1))
+
+dp = data_generator(fs, duration, wvf.df, ampl, filtering="whitening_bis", fcut=fcut, actPlot=TRUE)
 sprintf("noise and signal is high passed. Standard deviation: %g", sqrt(var(dp$y)))
 
