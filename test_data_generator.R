9cf51ee02ef733f4c41c2915ebed97ef16fb9473
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-05-15 01:04:55 +0200

    small changes

--- test_data_generator.R
+++ test_data_generator.R
@@ -18,11 +18,18 @@ wvf.df = data.frame("V1"=signalx,"V2"=signaly)
 d = data_generator1(fs, duration, wvf.df, ampl, filtering="prewhiten", actPlot=TRUE, verbose=TRUE)
 sprintf("noise and signal is whitened. Standard deviation: %g", sqrt(var(d$y)))
 
+d1 = data_generator1(fs, duration, wvf.df, ampl, filtering="spectrum", actPlot=TRUE, verbose=TRUE)
+sprintf("noise and signal is whitened. Standard deviation: %g", sqrt(var(d1$y)))
+
+plot(d$t,d$y, col="black", type="l")
+points(d1$t,d1$y, col="red", type="l")
+
+
 freq1 = fs*fftfreq(n)          # two-sided frequency vector
 freq = freq1[1:int(n/2)]
 wbFT = sqrt(fs)*fft(d$y)/(n*sqrt(n));
 
-plot(freq,2*abs(wbFT)[1:int(n/2)], log="y",type="l", xlab="frequency",ylab="ASD", col="cyan",ylim=c(1e-24,1e1))
+plot(freq,2*abs(wbFT)[1:int(n/2)], log="y",type="l", xlab="frequency",ylab="ASD", col="cyan",ylim=c(min(abs(wbFT)),5*max(abs(wbFT))))
 
 dp = data_generator(fs, duration, wvf.df, ampl, filtering="whitening_bis", fcut=fcut, actPlot=TRUE)
 sprintf("noise and signal is high passed. Standard deviation: %g", sqrt(var(dp$y)))
