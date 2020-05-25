efc9467be15d6f9ef037297a1db8ca20da72142d
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-05-25 08:52:45 +0200

    changes to be able to use all waveforms

--- g2mode_performance.R
+++ g2mode_performance.R
@@ -1,71 +1,32 @@
 source("specPdgrm.R")
 source("data_generator.R")
 source("functions.R")
+
 library(signal)
 
 ##################
 ### parameters ###
 ##################
+#name="s20-gw_10kpc16384"
+#name="s11.2--LS220--GravA"
+#name="s15.0--GShen--GravA"
+name="s15.0--LS220--GravA"
+#name="s15.0--SFHo--GravA"
+#name="s20.0--LS220--GravA"
+#name="s25.0--LS220--GravA"
+#   name="s40.0--LS220"
+
 fs=4096
-duration=1
 ampl=10
 filtering_method="prewhiten"
-
-######################
-### prepare signal ###
-######################
-s20 = read.table("../simulation_data/s20-gw_10kpc16384.dat"); # V1 time, V2 signal
-
-# signal sampled at 16384 Hz. Resampling at fs
-resamp_factor=16384/fs
-signaly=resample(s20$V2,1/16384,1/fs)
-
-# decimate time vector
-signalx=seq(1,(fs*duration),by=1)
-for (i in 1:(fs*duration)) {
-  signalx[i]=mean(s20$V1[((i-1)*resamp_factor+1):((i-1)*resamp_factor+resamp_factor)])
-}
-
-# remove times corresponding to the bounce for this wvf
-ind = which(signalx>0.28);
-signalx=signalx[ind]
-signaly=signaly[ind]
-
-wvf.df = data.frame("V1"=signalx,"V2"=signaly)
-
-
-####################################################################
-
-# determine the time interval where signal is non null
-s20_0 = s20[,2]!=0;  # identifying values == 0
-s20_0 = s20[s20_0,]; # discarding values
-
-timeGmode = s20_0[c(1,length(s20_0[,1])), 1];#time interval for signal different from 0
-
-###################
-### True ratios ###
-###################
-
-# For g2 modes, ratio (x variable in TF19) is x = Mpns / Rpns^2 
-true_data = read.table("../simulation_data/s20_data_g2.dat", sep = ",", comment.char = "#",header = TRUE); # true values
-colnames(true_data) = c ("time","x");
-
-# remove times corresponding to the bounce for this wvf
-true_data=subset(true_data, time >= 0.28)
-
-############################
-### FREQUENTIST ANALYSIS ###
-############################
 
 #############
 ### Model ###
 #############
-
+fits_data = read.table("inputs/A-A_fits_data_g2.dat", sep = ",");# data to generate model
+#fits_data = read.table("inputs/CoCo_fits_data_g2.dat", sep = ",");# data to generate model
 #fits_data = read.table("inputs/fits_data.dat", sep = ",");# data to generate model
-fits_data = read.table("../simulation_data/A-A_fits_data_g2.dat", sep = ",");# data to generate model
-#fits_data = read.table("../simulation_data/CoCo_fits_data_g2.dat", sep = ",");# data to generate model
 colnames(fits_data) = c("r", "f");
-
 
 # Linear model
 mod = lm(r~ 0 + f + I(f^3), data = fits_data);# final model
@@ -82,7 +43,23 @@ s2 = "~ f + I(f^2) - 1";
 Xm  = model.matrix(eval(parse(text=eval(parse(text=mu3)))), fits_data);
 Xs  = model.matrix(eval(parse(text=eval(parse(text=s2)))), fits_data);
 
+# Variable variance linear model
 fit = lmvar(fits_data$r, X_mu = Xm, X_sigma = Xs, intercept_mu = TRUE);
+
+#############################
+### Signal and true ratio ###
+#############################
+signal = signal_generator(name, fs)
+
+wvf.df=signal$wvf
+true_data=signal$true_data
+duration=signal$duration
+
+
+
+############################
+### FREQUENTIST ANALYSIS ###
+############################
 
 # g-mode estimate: starting from the right, left or median
 #gmode = c("left", "right", "median");
@@ -96,9 +73,11 @@ gmode = c("left");
 # loop over N generation of noisy data and add signal
 
 actplot=TRUE
-N=100
-dist_nb=20
+N=1
+dist_nb=25
 result<-matrix(nrow=N*dist_nb,ncol=12)
+
+set.seed(1)
 
 for (j in 1:dist_nb){
   dist = 1+(j-1)*.25
@@ -110,7 +89,7 @@ for (j in 1:dist_nb){
               um_L = 8, dm_L = 0, m_L = 8, initfreq_L = c(100, 500),
               um_R = 0, dm_R = 8, m_R = 8, initfreq_R = c(1000, 1700),
               gmode = gmode,
-              thruth_data=true_data, actPlot=FALSE,
+              thruth_data=true_data, actPlot=TRUE,
               limFreq = c(1000));
 
     out1 = covpbb(noisydata, mod=fit, l=200, p=90, fs=fs,
@@ -134,20 +113,28 @@ for (j in 1:dist_nb){
     result[i+(j-1)*N,10]=out1$residual[1,2]
     result[i+(j-1)*N,11]=out1$residual[1,3]
     result[i+(j-1)*N,12]=out1$residual[1,4]
-   
+    print(out1$covpbb)
   }
 }
 
-filename=sprintf("results_%s_fcut1000Hz_AA.txt", filtering_method)
+print(out)
+print(out1)
+
+filename=sprintf("results_%s_fcut1000Hz_AA_%s.txt", filtering_method, name)
 write.table(result, file=filename, sep=" ", row.names=FALSE, col.names=FALSE)
 
 #########################
 ### BAYESIAN ANALYSIS ###
 #########################
-  
+# determine the time interval where signal is non null
+#s20_0 = s20[,2]!=0;  # identifying values == 0
+#s20_0 = s20[s20_0,]; # discarding values
+
+#timeGmode = s20_0[c(1,length(s20_0[,1])), 1];#time interval for signal different from 0  
 # postSamples = read.table("postsamples.txt", header = T); # importing posterios samples
 # 
 # covpbbBayes(noisydata, postSamples, l=200, p=90, fs=fs, movGmode = 11, 
 #             um = 10, dm = 10, movBand = 5, timeGmode = NULL, 
 #             Obergaunlinguer_data, actPlot =TRUE, fmean = mean(fits_data$f))
+
 
