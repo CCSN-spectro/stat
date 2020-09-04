a98979bd8f8ac98eedc3f4067799f1b79d922915
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-09-04 15:06:22 +0200

    Update

--- PSD/spectrum.py
+++ PSD/spectrum.py
@@ -23,6 +23,9 @@ ET_C_psd=np.loadtxt('ET_C_sensitivity.txt', skiprows=0)
 # Get CE spectrum
 CE1_psd=np.loadtxt('curves_Jan_2020/ce1.txt', skiprows=0)
 
+# Get CE spectrum
+CE2_psd=np.loadtxt('curves_Jan_2020/ce2.txt', skiprows=0)
+
 # Get aLIGO spectrum
 AL_psd=np.loadtxt('ALIGO_sensitivity.txt', skiprows=7)
 
@@ -43,6 +46,12 @@ freq_CE1_psd = np.arange(5., fNCE, deltaf)
 psd = interpolate.interp1d(CE1_psd[:,0],CE1_psd[:,1])
 CE1_psd_res = psd(freq_CE1_psd)
 
+fNCE=CE2_psd[-1,0];                       # Nyquist freq
+freq_CE2_psd = np.arange(5., fNCE, deltaf)
+psd = interpolate.interp1d(CE2_psd[:,0],CE2_psd[:,1])
+CE2_psd_res = psd(freq_CE2_psd)
+
+
 freq_AL_psd = np.arange(9., 8000., deltaf)
 psd = interpolate.interp1d(AL_psd[:,0],AL_psd[:,6])
 AL_psd_res = psd(freq_AL_psd)
@@ -59,8 +68,9 @@ plt.loglog(ET_D_psd[:,0],ET_D_psd[:,3],'green',label='ET-D HF+LF')
 plt.loglog(ET_B_psd[:,0],ET_B_psd[:,1],'red',label='ET-B')
 plt.loglog(ET_C_psd[:,0],ET_C_psd[:,1],'k',label='ET-C')
 plt.loglog(CE1_psd[:,0],CE1_psd[:,1],'cyan',label='CE stage1')
+plt.loglog(CE2_psd[:,0],CE2_psd[:,1],'m',label='CE stage2')
 plt.loglog(AL_psd[:,0],AL_psd[:,6],'b',label='aLIGO design')
-plt.loglog(ADV_psd[:,0],ADV_psd[:,6],'m',label='ADV design')
+#plt.loglog(ADV_psd[:,0],ADV_psd[:,6],'m',label='ADV design')
 
 plt.axis([1, fmax, 1e-25, 4e-20])
 
