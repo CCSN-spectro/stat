9ccdefa0870f801bc98d1d68ef80916ae23dfe31
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-09-14 01:30:49 +0200

    remove a sqrt(2) factor that was wrong when computing the SNR

--- computeSNR_allwvf.R
+++ computeSNR_allwvf.R
@@ -3,14 +3,16 @@ source ("data_simulation.R")
 dist=10    # kpc
 fmin=10    # Hz
 
-detectors=c("aLIGO", "ADV", "KAGRA", "CE1", "CE2", "ET_B", "ET_C", "ET_D")
-signals=c("s20.0--SFHo", "s11.2--LS220", "s15.0--GShen", "s15.0--LS220", 
-          "s15.0--SFHo", "s20.0--LS220", "s25.0--LS220", "s40.0--LS220")
+detectors=c("ALIGO", "ADV", "KAGRA", "CE1", "CE2", "ET_B", "ET_C", "ET_D")
+signals=c("s11.2--LS220", "s15.0--LS220", "s15.0--SFHo", "s15.0--GShen", 
+          "s20.0--LS220", "s20.0--SFHo", "s25.0--LS220", "s40.0--LS220")
 
 for (det in detectors){
   print(det)
   for (sig in signals){
-     compute_SNR(sig, det, fmin, dist)
+     snr=compute_SNR(sig, det, fmin, dist)
+     
+     print(sprintf('%s snr=%.1f', sig, snr))
   }
 }
 
