7d89d44e765745f4220348cdec30b4d732cdafed
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-09-23 17:31:50 +0200

    Add an option to compute SNR for wvf without 100ms after bounce

--- computeSNR_allwvf.R
+++ computeSNR_allwvf.R
@@ -3,16 +3,19 @@ source ("data_simulation.R")
 dist=10    # kpc
 fmin=10    # Hz
 
-detectors=c("ALIGO", "ADV", "KAGRA", "CE1", "CE2", "ET_B", "ET_C", "ET_D")
+detectors=c("aLIGO", "ADV", "KAGRA", "CE1", "CE2", "ET_B", "ET_C", "ET_D")
 signals=c("s11.2--LS220", "s15.0--LS220", "s15.0--SFHo", "s15.0--GShen", 
           "s20.0--LS220", "s20.0--SFHo", "s25.0--LS220", "s40.0--LS220")
 
+#signals=c("s15.0--LS220", "s15.0--SFHo", "s15.0--GShen")
+#det="aLIGO"
+#signals="s11.2--LS220"
+
 for (det in detectors){
-  print(det)
+print(det)
   for (sig in signals){
-     snr=compute_SNR(sig, det, fmin, dist)
-     
-     print(sprintf('%s snr=%.1f', sig, snr))
+    snr=compute_SNR(sig, det, fmin, dist, pbOff=TRUE)
+    print(sprintf('%s snr=%.3f', sig, snr))
   }
 }
 
